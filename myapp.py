3b6c104ea7dc5c4a1db128118725ec039697799c
amo95 <jameaamo@gmail.com>
2019-07-23 11:32:45 +0100

    update

--- myapp.py
+++ myapp.py
@@ -9,6 +9,8 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 
+
+app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -28,7 +30,6 @@ GPIO.output(ledRed, GPIO.LOW)
 GPIO.output(ledYellow, GPIO.LOW)
 GPIO.output(ledGreen, GPIO.LOW)
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'Thisisgroup3secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/pi/Desktop/Pi-JED/database.db'
