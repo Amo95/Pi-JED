3355199c8319a2b4768e20caadd04b2b705b1816
amo95 <jameaamo@gmail.com>
2019-07-25 23:42:25 +0000

    new

---
+++ static/dashboard.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+	var nav = false;
+	function show_hide_nav(id){
+	var navbar = document.getElementById(id);
+
+	if(nav == false){
+			navbar.style.display = "block";
+			nav = true;
+		}else{
+			navbar.style.display = "none";
+			nav = false;
+		}
+	}
+</script>
