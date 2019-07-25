215e5b28d0f24f34e969a70289f0b4feb13ab5e7
amo95 <jameaamo@gmail.com>
2019-07-25 00:29:58 +0000

    new

--- myapp.py
+++ myapp.py
@@ -90,7 +90,7 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
-        return '<a href="/login">Login</a>'
+        return '<p>you have successfully signup. Click on <a href="/login">Login</a> to sign in</p>'
         return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
